a6841a77c2e86493f809a5b36e664aa57d3f3488
Franco M <damianmaciel0@gmail.com>
2022-11-17 18:59:28 -0300

    change fs imports to node:fs

--- src/utils/removeFiles.ts
+++ src/utils/removeFiles.ts
@@ -1,10 +1,10 @@
-import fs from 'fs'
+import fs from 'node:fs'
 import path from 'path'
 
 export function removeFiles (filesDir: string): void {
   fs.readdirSync(filesDir)?.forEach(fileName => {
     fs.unlink(path.join(filesDir, fileName), (err) => {
-      if (err != null) throw err
+      if (err !== null) throw err
       console.log(`${fileName} was deleted`)
     })
   })
