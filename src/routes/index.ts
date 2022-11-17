d3618b5cd4f75eca61fcde1e054a0180e7d5adcf
Franco M <damianmaciel0@gmail.com>
2022-11-17 20:00:47 -0300

    chnage path access

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import path from 'node:path'
+
 import fs from 'node:fs/promises'
 
 const router = Router()
@@ -9,6 +11,8 @@ const rootPath = pathname.slice(1)
 
 const PATH_ROUTER = `${rootPath}`
 
+console.log({ PATH_ROUTER, pathname })
+
 function removeExtension (fileName: string): string {
   const file = fileName.split('.')[0]
   return file
@@ -17,13 +21,14 @@ function removeExtension (fileName: string): string {
 async function readFiles (path: string) {
   try {
     const files = await fs.readdir(path)
+    console.log({ files })
     return files
   } catch (error: any) {
     console.error(error)
   }
 }
 
-void readFiles(PATH_ROUTER).then(files => {
+void readFiles(path.join('dist', 'routes')).then(files => {
   files?.forEach((fileName) => {
     const cleanName = removeExtension(fileName)
     if (cleanName !== 'index') {
