89d6f123a958b560a92e4f7fdce9ddc535b91739
Franco M <damianmaciel0@gmail.com>
2022-11-17 19:43:59 -0300

    change fs to fs promises

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 const router = Router()
 
@@ -14,15 +14,37 @@ function removeExtension (fileName: string): string {
   return file
 }
 
-fs.readdirSync(PATH_ROUTER).forEach((fileName) => {
-  const cleanName = removeExtension(fileName)
-  if (cleanName !== 'index') {
-    import(`./${cleanName}.js`)
-      .then(moduleRouter => {
-        console.log(`Se está cargando la ruta: /${cleanName}`)
-        router.use(`/${cleanName}`, moduleRouter.router)
-      }).catch(console.error)
+async function readFiles (path: string) {
+  try {
+    const files = await fs.readdir(path)
+    return files
+  } catch (error: any) {
+    console.error(error)
   }
+}
+
+void readFiles(PATH_ROUTER).then(files => {
+  files?.forEach((fileName) => {
+    const cleanName = removeExtension(fileName)
+    if (cleanName !== 'index') {
+      import(`./${cleanName}.js`)
+        .then(moduleRouter => {
+          console.log(`Se está cargando la ruta: /${cleanName}`)
+          router.use(`/${cleanName}`, moduleRouter.router)
+        }).catch(console.error)
+    }
+  })
 })
+
+// fs.readdirSync(PATH_ROUTER).forEach((fileName) => {
+//   const cleanName = removeExtension(fileName)
+//   if (cleanName !== 'index') {
+//     import(`./${cleanName}.js`)
+//       .then(moduleRouter => {
+//         console.log(`Se está cargando la ruta: /${cleanName}`)
+//         router.use(`/${cleanName}`, moduleRouter.router)
+//       }).catch(console.error)
+//   }
+// })
 
 export { router }
